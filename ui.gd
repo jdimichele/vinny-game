57025dffa1eff2fdae6e67e6c567d6d253aa1082
justin.dimichele <justindimichele@gmail.com>
2026-04-21 21:37:21 -0700

    Messed around with some UI, adding player_stats

--- ui.gd
+++ ui.gd
@@ -2,24 +2,23 @@ extends CanvasLayer
 
 @onready var player_level_label: Label = $PlayerLevel/VBoxContainer/Label
 @onready var player_level_icon: TextureRect = $PlayerLevel/VBoxContainer/TextureRect
-@onready var experience_label: Label = $Experience/VBoxContainer/Label
-@onready var experience_icon: TextureRect = $Experience/VBoxContainer/TextureRect
-@onready var health_bar: TextureProgressBar = $MarginContainer/TextureProgressBar
+@onready var health_bar: TextureProgressBar = $HealthProgressContainer/HealthProgressBar
+@onready var experience_bar: TextureProgressBar = $ExperienceProgressContainer/ExperienceProgressBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Globals.connect("stat_change", update_stat_text)
 
 func update_stat_text():
-	update_health_text()
-	update_experience_text()
+	update_health_bar()
+	update_experience_bar()
 	update_player_level_text()
 
-func update_health_text():
+func update_health_bar():
 	health_bar.value = Globals.health
 
-func update_experience_text():
-	experience_label.text = str(Globals.experience)
+func update_experience_bar():
+	experience_bar.value = Globals.experience
 
 func update_player_level_text():
 	player_level_label.text = str(Globals.player_level)
